a670228d87acf72a3f613439d3044d65ce2f6700
schinken <schinken@hackerspace-bamberg.de>
2012-09-28 21:10:41 +0200

    changed order of min and max

--- common.js
+++ common.js
@@ -2,7 +2,7 @@ module.exports = {
     'map_range': function( val, min1, max1, min2, max2 ) {
         return min2 + (max2 - min2) * ((val - min1) / (max1 - min1));
     },
-    'clamp': function( val, max, min ) {
+    'clamp': function( val, min, max ) {
         if( val > max ) {
             return max;
         }    
